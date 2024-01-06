d14f8634de6b8d1928cdca98d3cbf48d940d3fd8
Your Name <you@example.com>
2024-01-07 04:32:19 +1000

    third step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import prompt
+from brain_games.cli import welcome_user
 def welcome():
     print('Welcome to the Brain Games!')
+    welcome_user()
 def main():
     welcome()
-if __name__ == '__main__':
+if __name__ == '__main__':     
     main()
